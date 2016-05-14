b393558791ddf0d28249e7e964deba9a943d1a6c
lxfly2000 <gaoboyuan8@qq.com>
2016-05-14 02:19:41 -0500

    增加自动编译脚本

--- MIDIPlayer/build.bat
+++ MIDIPlayer/build.bat
@@ -1,4 +1,6 @@
 @echo off
+if "%1"=="C" goto Clean
+if "%1"=="c" goto Clean
 set _MSBuild_=%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe
 setlocal EnableDELAYEDEXPANSION
 if not exist "!_MSBuild_!" goto _NO_MSBuild_
@@ -26,3 +28,25 @@ echo 你需要安装 VS2015 才能运行该脚本。
 goto End
 :End
 pause
+goto EOL
+:Clean
+cd midifile
+rd /s /q Debug
+rd /s /q lib
+rd /s /q lib_d
+rd /s /q lib_x64
+rd /s /q lib_x64_d
+rd /s /q Release
+rd /s /q x64
+cd..
+rd /s /q Debug
+rd /s /q lib
+rd /s /q lib_d
+rd /s /q lib_x64
+rd /s /q lib_x64_d
+rd /s /q Release
+rd /s /q TestMidiPlayer_x64
+rd /s /q x64
+echo 完成。
+ping -n 2 127.8>nul
+:EOL
